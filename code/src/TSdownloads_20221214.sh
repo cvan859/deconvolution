945805d116cdf0c39a1a2aa9462eeead086fef59
Cassandra Van <cassandv@uci.edu>
2022-12-15 00:21:58 -0800

    Use script to download selected RNA-seq data from Tabula Sapiens.

---
+++ code/src/TSdownloads_20221214.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+# means not needed
+## means already downloaded
+
+#wget 'https://figshare.com/ndownloader/files/34701958' -O scBladder.h5ad.zip #bladder
+wget 'https://figshare.com/ndownloader/files/34701964' -O scBlood.h5ad.zip #blood
+wget 'https://figshare.com/ndownloader/files/34701967' -O scBoneMarrow.h5ad.zip #bone marrow
+#wget 'https://figshare.com/ndownloader/files/34701970' -O scEye.h5ad.zip #eye
+wget 'https://figshare.com/ndownloader/files/34701973' -O scFat.h5ad.zip #fat
+wget 'https://figshare.com/ndownloader/files/34701979' -O scKidney.h5ad.zip #kidney
+wget 'https://figshare.com/ndownloader/files/34701982' -O scLargeInstestine.h5ad.zip #large intestine
+#wget 'https://figshare.com/ndownloader/files/34701991' -O scLung.h5ad.zip #lung
+wget 'https://figshare.com/ndownloader/files/34701994' -O scLymphNode.h5ad.zip #lymph node
+#wget 'https://figshare.com/ndownloader/files/34701997' -O scMammary.h5ad.zip #mammary
+##wget 'https://figshare.com/ndownloader/files/34702000' -O scMuscle.h5ad.zip #muscle
+wget 'https://figshare.com/ndownloader/files/34702003' -O scPancreas.h5ad.zip #pancreas
+wget 'https://figshare.com/ndownloader/files/34702006' -O scProstate.h5ad.zip #prostate
+#wget 'https://figshare.com/ndownloader/files/34702009' -O scSalivaryGland.h5ad.zip #salivary_gland
+#wget 'https://figshare.com/ndownloader/files/34702012' -O scSkin.h5ad.zip #skin
+##wget 'https://figshare.com/ndownloader/files/34702015' -O scSmallIntestine.h5ad.zip #small intestine
+##wget 'https://figshare.com/ndownloader/files/34702018' -O scSpleen.h5ad.zip #spleen
+wget 'https://figshare.com/ndownloader/files/34702027' -O scThymus.h5ad.zip #thymus
+#wget 'https://figshare.com/ndownloader/files/34702030' -O scTongue.h5ad.zip #tongue
+#wget 'https://figshare.com/ndownloader/files/34702033' -O scTrachea.h5ad.zip #trachea
+wget 'https://figshare.com/ndownloader/files/34702036' -O scUterus.h5ad.zip #uterus
+wget 'https://figshare.com/ndownloader/files/34702039' -O scVasculature.h5ad.zip #vasculature
+#wget 'https://figshare.com/ndownloader/files/34702042' -O scEndothelial.h5ad.zip #endothelial
+#wget 'https://figshare.com/ndownloader/files/34702048' -O scEpithelial.h5ad.zip #epithelial
+#wget 'https://figshare.com/ndownloader/files/34702051' -O scGermLine.h5ad.zip #germ line
+#wget 'https://figshare.com/ndownloader/files/34702054' -O scStromal.h5ad.zip #stromal
+wget 'https://figshare.com/ndownloader/files/34702069' -O scImmune.h5ad.zip #immune
+#wget 'https://figshare.com/ndownloader/files/34702114' -O scFullTSRNA.h5ad.zip #full Tabula Sapiens
+
+# c(Adipose_Subcutaneous="Adipose - Subcutaneous",
+#       Adipose_Visceral = "Adipose - Visceral (Omentum)",
+#       Adrenal_Gland = "Adrenal Gland",
+#       Artery_Aorta = "Artery - Aorta",
+#       Artery_Coronary = "Artery - Coronary",
+#       Brain_Hippocampus = "Brain - Hippocampus",
+#       Brain_Hypothalamus = "Brain - Hypothalamus",
+#       Colon_Transverse = "Colon - Transverse",
+#       Colon_Sigmoid = "Colon - Sigmoid",
+# #      Heart_Left_Ventricle = "Heart - Left Ventricle",
+#       Kidney_Cortex = "Kidney - Cortex",
+# #      Liver = "Liver",
+#       Lung = "Lung",
+# #      Muscle_Skeletal = "Muscle - Skeletal",
+# #      Spleen = "Spleen",
+# #      Small_Intestine_Terminal_Ileum = "Small Intestine - Terminal Ileum",
+#       Stomach = "Stomach",
+#       Thyroid = "Thyroid",
+#       Pancreas = "Pancreas",
+#       Pituitary = "Pituitary")
